23cffd13d6752ce5fa3a0b0a110e40509b9b6aeb
bazoupolo <129897987+bazoupolo@users.noreply.github.com>
2023-05-02 15:58:04 -0400

    index
    player

---
+++ the orville.js
@@ -0,0 +1,21 @@
+console.log{"Bonjour le monde"}
+
+var nom = "Thaddey"
+
+if { 1+1==2 } {
+    console.log{"c'est thaddey "}
+    document.body.style = 
+} else {
+
+    console.log{"c'est pas thaddey "}
+
+
+}
+
+function direBonjour {}  {
+
+    console.log{"bonjour " + nom }
+
+
+}
+{}
